c1e587e03ab39289492ee628e40686546140e327
Goga Barabadze <goga.barabadze73@gmail.com>
2021-12-07 09:16:11 +0100

    Initial Commit

---
+++ Big Five Personality Traits/Big_Five_Personality_TraitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Big_Five_Personality_TraitsApp.swift
+//  Big Five Personality Traits
+//
+//  Created by Goga Barabadze on 07.12.21.
+//
+
+import SwiftUI
+
+@main
+struct Big_Five_Personality_TraitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
